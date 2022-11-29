276e323a548dc952a05f66aa6204c1bbfee54b32
Vishnu <vishnuvs5171@gmail.com>
2022-11-30 04:49:32 +0530

    Committing stable version Update and Delete operation

--- Assignments JDBC/src/assignment4/jdbc/AllJdbcOpInOne.java
+++ Assignments JDBC/src/assignment4/jdbc/AllJdbcOpInOne.java
@@ -13,12 +13,13 @@ public class AllJdbcOpInOne {
 		Connection connection = null;
 		ResultSet resultSet = null;
 		Statement statement = null;
-		Scanner sc=null;
-		int s_id=0;
-		String Name=null;
-		int c_id=0;
-		int Age=0;
+		Scanner sc = null;
+		int s_id = 0;
+		String Name = null;
+		int c_id = 0;
+		int Age = 0;
 		int userChoiceMain = 0;
+		String insertQuery = null;
 //get the input from the user ,Which choice user wants
 		userChoiceMain = JdbcUtil1.getUserInputForSelectQuery();
 //gets the connection here and assigning the connection to local variable connection
@@ -28,31 +29,33 @@ public class AllJdbcOpInOne {
 				statement = connection.createStatement();
 				// here i used switch statement to perform the operation based on user's choice
 				switch (userChoiceMain) {
-				//1 is for Create/Insert Operation
+				// 1 is for Create/Insert Operation
 				case 1: {
 					if (statement != null) {
-						 sc = new Scanner(System.in);
+						sc = new Scanner(System.in);
 						System.out.println("Please provide the required details to create the query");
 						System.out.println("Enter the Stud_Id :");
-						 s_id = sc.nextInt();
+						s_id = sc.nextInt();
 						System.out.println("Enter the Stud_Name :");
-						 Name = sc.next();
+						Name = sc.next();
 						System.out.println("Enter the Course_Id :");
-						 c_id = sc.nextInt();
+						c_id = sc.nextInt();
 						System.out.println("Enter the Stud_Age :");
-						 Age = sc.nextInt();
+						Age = sc.nextInt();
 
-String InsertQuery = String.format("Insert into student values('%d','%s','%d','%d')",s_id,Name,c_id,Age);
-//String.format("Insert into student values (`%d`,`%s`,`%d`,`%d`)",s_id,Name,c_id,Age);
-					 			
+						String InsertQuery = String.format("Insert into student values('%d','%s','%d','%d')", s_id,
+								Name, c_id, Age);
+//the below code is not working because of ` symbol i think!!
+						// String.format("Insert into student values
+						// (`%d`,`%s`,`%d`,`%d`)",s_id,Name,c_id,Age);
 						int rowCount = statement.executeUpdate(InsertQuery);
 						// Getting the output for insert operation
 						System.out.println("No of rows affected :: " + rowCount);
-						//JdbcUtil1.closeConnections(resultSet, statement, connection);
+						// JdbcUtil1.closeConnections(resultSet, statement, connection);
 					}
 					break;
 				}
-				//2 is for Select/Read Operation
+				// 2 is for Select/Read Operation
 				case 2: {
 
 					if (statement != null) {
@@ -74,9 +77,81 @@ String InsertQuery = String.format("Insert into student values('%d','%s','%d','%
 					break;
 				}
 				case 3: {
+					if (statement != null) {
+						sc = new Scanner(System.in);
+						System.out.println("Please provide the required details to create the query");
+						System.out.println(
+								"Which field do you want to update?\r\n 1.Name\t2.c_id\t3.Age\t4.All the 3 fileds");
+						int updateOption = sc.nextInt();
+						if (updateOption == 1) {
+							System.out.println("Enter the Stud_id whose name you want to  change : ");
+							s_id = sc.nextInt();
+							System.out.println("Enter the new Stud_Name :");
+							Name = sc.next();
+							Name="'"+Name+"'";
+							insertQuery = "Update student set Name ="+Name+" where s_Id="+s_id+";";
+						}
+						if (updateOption == 2) {
+							System.out.println("Enter the Stud_id whose CourseId you want to  change : ");
+							s_id = sc.nextInt();
+							System.out.println("Enter the new Course_Id :");
+							c_id = sc.nextInt();
+							insertQuery = String.format("Update student set c_id='%d' where s_id='%d'", c_id,s_id);
+						}
+						if (updateOption == 3) {
+							System.out.println("Enter the Stud_id whose age you want to  change : ");
+							s_id = sc.nextInt();
+							System.out.println("Enter the new Stud_Age :");
+							Age = sc.nextInt();
+							insertQuery = String.format("Update student set Age='%d' where s_id='%d'", Age,s_id);
+						}
+						if (updateOption == 4) {
+							System.out.println("Enter the Stud_id whose details you want to  change : ");
+							s_id = sc.nextInt();
+							System.out.println("Enter the new Stud_Name :");
+							Name = sc.next();
+							System.out.println("Enter the new Course_Id :");
+							c_id = sc.nextInt();
+							System.out.println("Enter the new Stud_Age :");
+							Age = sc.nextInt();
+							insertQuery = String.format("Update student set Name='%s', c_id='%d' ,Age='%d' where s_id='%d'", Name,c_id,Age,s_id);
+						}
+
+						 
+
+//the below code is not working because of ` symbol i think!!
+						// String.format("Insert into student values
+						// (`%d`,`%s`,`%d`,`%d`)",s_id,Name,c_id,Age);
+						int rowCount = statement.executeUpdate(insertQuery);
+						// Getting the output for insert operation
+						System.out.println("No of rows affected :: " + rowCount);
+						// JdbcUtil1.closeConnections(resultSet, statement, connection);
+					}
 					break;
 				}
 				case 4: {
+					if (statement != null) {
+						sc = new Scanner(System.in);
+						System.out.println("Please provide the required details to create the query");
+						System.out.println("Enter the Stud_Id :");
+						s_id = sc.nextInt();
+						System.out.println("Enter the Stud_Name :");
+						Name = sc.next();
+						System.out.println("Enter the Course_Id :");
+						c_id = sc.nextInt();
+						System.out.println("Enter the Stud_Age :");
+						Age = sc.nextInt();
+
+						String InsertQuery = String.format("Insert into student values('%d','%s','%d','%d')", s_id,
+								Name, c_id, Age);
+//the below code is not working because of ` symbol i think!!
+						// String.format("Insert into student values
+						// (`%d`,`%s`,`%d`,`%d`)",s_id,Name,c_id,Age);
+						int rowCount = statement.executeUpdate(InsertQuery);
+						// Getting the output for insert operation
+						System.out.println("No of rows affected :: " + rowCount);
+						// JdbcUtil1.closeConnections(resultSet, statement, connection);
+					}
 					break;
 				}
 
@@ -89,8 +164,8 @@ String InsertQuery = String.format("Insert into student values('%d','%s','%d','%
 			se.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
-			if(sc!=null) {
+		} finally {
+			if (sc != null) {
 				sc.close();
 			}
 		}
